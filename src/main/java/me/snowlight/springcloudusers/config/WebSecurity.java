99f1e97d3346ff39183d24f1bd3541d930964b71
private desktop pc <it.s.aemt@gmail.com>
2022-07-23 17:16:57 +0900

    Spring cloud actuator 설정

--- src/main/java/me/snowlight/springcloudusers/config/WebSecurity.java
+++ src/main/java/me/snowlight/springcloudusers/config/WebSecurity.java
@@ -30,7 +30,7 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
-        // http.authorizeRequests().antMatchers("/users/**").permitAll();
+        http.authorizeRequests().antMatchers("/actuator/**").permitAll();
         http.authorizeRequests().antMatchers("/**")
             .hasIpAddress("192.168.0.5")
             .and().addFilter(getAuthentionFilter());
